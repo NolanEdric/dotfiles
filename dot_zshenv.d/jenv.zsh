95caaebced3d874c3b6803b4a40f95e7c5d25ad7
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2021-11-09 00:37:13 +0700

    added haproxy config

--- dot_zshenv.d/jenv.zsh
+++ dot_zshenv.d/jenv.zsh
@@ -1,7 +1,7 @@
-jenv() {
-    unset -f jenv
-    export PATH="$HOME/.jenv/bin:$PATH"
-    eval "$(jenv init -)"
-}
+# jenv() {
+#     unset -f jenv
+#     export PATH="$HOME/.jenv/bin:$PATH"
+#     eval "$(jenv init -)"
+# }
 
-export PATH="$PATH:$HOME/downloads/JDKs/jdk1.8.0_301/bin"
+# export PATH="$PATH:$HOME/downloads/JDKs/jdk1.8.0_301/bin"
