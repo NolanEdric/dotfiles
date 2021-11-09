4b5f6bd9c7f29ed199b5caf6123107022fe2f407
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2021-11-10 02:23:18 +0700

    removed vi/vim/nvim aliases

--- dot_zprofile.d/start_services.zsh
+++ dot_zprofile.d/start_services.zsh
@@ -10,3 +10,11 @@
 #         sudo service $svc start 1>/dev/null
 #     fi
 # done
+
+enabled_svc=$(systemctl list-unit-files | awk -F '.' '/.service|.timer/ && /enabled/{print $1}' -)
+running_svc=$(systemctl | awk -F '.' '/.service|.timer/ && /running/{print $1}' -)
+
+comm -23 <(echo $enabled_svc) <(echo $running_svc) | while read svc
+do
+  sudo systemctl start $svc
+done
--- scripts/executable_bootstrap.sh
+++ scripts/executable_bootstrap.sh
@@ -1,14 +1,19 @@
+echo 'EDITOR=micro' | sudo tee -a /etc/environment
+
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # sudo apt install autojump -y
 
-sudo pacman -S autojump
+# sudo pacman -S autojump
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
