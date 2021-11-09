0823f893cf69d205472ba7b67a6bb52ba4b21182
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2021-11-10 02:57:26 +0700

    added setup for micro

--- scripts/executable_bootstrap.sh
+++ scripts/executable_bootstrap.sh
@@ -1,4 +1,8 @@
-echo 'EDITOR=micro' | sudo tee -a /etc/environment
+echo 'export EDITOR=micro' | sudo tee -a /etc/zsh/zprofile
+
+sudo mkdir -p /root/.config/micro
+
+sudo cp -vfR "$HOME/.config/micro/settings.json" "/root/.config/micro/settings.json"
 
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -14,6 +18,7 @@ echo 'EDITOR=micro' | sudo tee -a /etc/environment
 
 # git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+#  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 
